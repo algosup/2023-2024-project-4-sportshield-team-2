88da08a3132d3f4527be4af96d76bd3c9b0e8e52
EnzoGuillouche <enzo.guillouche@algosup.com>
2024-03-28 16:30:57 +0100

    Update tests.cpp
    Created a better understandable and visually appealing interface to read and create unit tests for the GitHub actions.

--- test/src/tests.cpp
+++ test/src/tests.cpp
@@ -1,24 +1,47 @@
 #include <iostream>
 using namespace std;
 
-/* test to check the ledPin
-you just have to change the variables to check multiple cases.
-*/ 
-const int trueLedPin = 11;
-const int falseLedPin = 10;
+// test to check the ledPin
+bool ledPinTest(const int ledPin){
+    if (ledPin == 11) { // expectation
+        return true; // return true if corresponding to the expectation
+    } else {
+        return false; // return false if not corresponding to the expectation
+    }
+}
+
 
 void allTests() {
-    if (trueLedPin == 11) {
-        cout << "The expected true led pin is 11" << endl;
-    } else {
-        cout << "The expected true led pin is not 11" << endl;
+    // the template should be reused each time we add another test
+    cout << "[==========] Running unit tests" << endl; // visuals regarding tests and actions
+    const int ledPin = 11; // you just have to change the variables to check multiple cases.
+    cout << "[----------] Run in Linux server" << endl;  // *** COPY PASTE THE TEMPLATE FROM HERE ***
+    cout << "[ RUN      ] Run from ledPinTest" << endl; // name of the test 
+    cout << "[          ] " << endl; 
+    cout << "[  Results ] Display the results" << endl; 
+    if (ledPinTest(ledPin) == true){ // call the test and print the result depending on the expectation
+        cout << "[  PASSED  ]" << endl;
+    } 
+    else{
+        cout << "[  FAILED  ]" << endl;
     }
-    if (falseLedPin == 11){
-        cout << "The expected false led pin is 11" << endl;
+    // other test of the same function
+    const int incorrectLedPin = 10;
+    cout << "[----------]" << endl; 
+    cout << "[ RUN      ] Run from ledPinTest" << endl; // name of the test 
+    cout << "[          ] " << endl; 
+    cout << "[  Results ] Display the results" << endl; 
+    if (ledPinTest(incorrectLedPin) == true){ 
+        cout << "[  PASSED  ]" << endl;
+    } 
+    else{
+        cout << "[  FAILED  ]" << endl;
     }
-    else {
-        cout << "The expected false led pin is not 11" << endl;
-    }
+    // here come the other tests regarding the same function
+    cout << "[----------] 2 test from ledPinTest" << endl; // the number of tests from the function
+    cout << "" << endl; // put this at the end of the test regarding a function (when you switch to another function)
+    // here come other tests
+    cout << "[==========] End unit tests" << endl; // visuals regarding tests and actions
 }
 
 // main function, where we call the test function
